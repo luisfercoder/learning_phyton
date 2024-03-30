a0146f7600a654d916ef22daf97433da9737c5fd
Luis Fernando <luisfercoder@gmail.com>
2024-03-30 17:27:32 -0600

    Collections and list

---
+++ linkedin_course/challenge4.py
@@ -0,0 +1,18 @@
+#creating a list and dictionary
+stars=[
+  'sol',
+  'Alpha Centuri',
+  'Barnard',
+  'Wolf 359',
+]
+peak={
+  'African':'Kilimanjaro',
+  'Antarctic':'Vinson',
+  'Australian':'Punck Jaya',
+  'Eurasian':'Everest',
+  'Noth_America':'Denali',
+  'Pacific':'Mauku Kea',
+  'South_American':'Aconcagua',
+}
+print(stars[3])
+print(peak['Pacific'])
---
+++ linkedin_course/list.py
@@ -0,0 +1,14 @@
+#creating my fisrt simple list
+cities =[
+  'tokyo',
+  'munbai',
+  'sapin',
+]
+#Call a value of the list
+print(cities[2])
+#more complex  dictionary
+california_symbols ={
+  'bird':'California quail',
+  'animal':'Grizzly bear',
+  'fruit':'Avocado',
+}
